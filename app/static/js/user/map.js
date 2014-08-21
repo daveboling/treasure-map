30489561b3ec48ad48f54593625e8285953fd44e
David D. Boling <daviddboling@gmail.com>
2014-08-20 21:31:19 -0500

    Added Google Waypoints

--- app/static/js/user/map.js
+++ app/static/js/user/map.js
@@ -5,12 +5,33 @@
     var waypoints = $.makeArray(arr);
 
     //Loops through all divs and grabs the lat and lng from the data attributes
-    waypoints.map(function(div){
-       var lat = $(div).attr('data-lat');
-       var lng = $(div).attr('data-lng');
-      return {lat: lat, lng: lng};
+    waypoints = waypoints.map(function(div){
+       var lat  = $(div).attr('data-lat');
+       var lng  = $(div).attr('data-lng');
+       var name = $(div).text();
+      return {lat: lat, lng: lng, name: name};
     });
 
+    //Initialize map with first waypoint
+    initMap(waypoints[0].lat, waypoints[1].lng, 2);
+
+    //Data for Google Maps pins
+    waypoints.forEach(function(a){
+      addMarker(a.lat, a.lng, a.name);
+    });
+
+    var wp = waypoints.map(function(waypoint){
+    	return new google.maps.LatLng(waypoint.lat, waypoint.lng);
+    });
+
+    var start = wp[0];
+    var end   = wp[length];
+
+    //console.log(wp); console.log(start); console.log(end);
+
   });
+
+
+
 
 })();
